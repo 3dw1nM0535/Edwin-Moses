48bd7506e7f0438ee05847736c93594b56004589
3dw1nM0535 <workockmoses@gmail.com>
2018-03-27 21:18:45 +0300

    server init&private env setup

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import path from "path";
+import webpack from "webpack";
+import webpackHotMiddleware from "webpack-hot-middleware";
+import webpackDevMiddleware from "webpack-dev-middleware";
+
+import config from "../webpack.config";
+import privateEnv from "./config/private_env";
+
+config.plugins.push(new webpack.HotModuleReplacementPlugin());
+
+const compiler = webpack(config);
+
+mongoose.connect(privateEnv.MONGO_URI);
+
+const app = express();
+const port = privateEnv.PORT;
+
+app.use(bodyParser.json());
+app.use(webpackDevMiddleware(compiler, {
+  noInfo: true,
+  hot: true,
+  historyApiFallback: true,
+  publicPath: config.output.publicPath,
+}));
+app.use(webpackHotMiddleware(compiler));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(port);
