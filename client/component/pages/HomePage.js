73ed9a892b8c068077fcca8f274eeaddc831fabb
3dw1nM0535 <workockmoses@gmail.com>
2018-03-27 21:16:24 +0300

    homepage component

---
+++ client/component/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Grid, Header } from "semantic-ui-react";
+
+const HomePage = () => (
+  <Grid centered columns={2} container>
+    <Grid.Column>
+      <Header as="h1">Edwin Moses(Full Stack Developer)</Header>
+    </Grid.Column>
+  </Grid>
+);
+
+export default HomePage;
