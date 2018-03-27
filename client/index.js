f7384cf29ea4cc62a1432281a0c1a516634d8168
3dw1nM0535 <workockmoses@gmail.com>
2018-03-27 21:16:05 +0300

    defined home route

---
+++ client/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { Router } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+
+import store, { history } from "./store/store";
+import App from "./App";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root"),
+);
+
+if (module.hot) {
+  module.hot.accept("./App", () => {
+    const NextApp = require("./App").default;
+    ReactDOM.render(
+      <Provider store={store}>
+        <Router history={history}>
+          <NextApp />
+        </Router>
+      </Provider>,
+      document.getElementById("root"),
+    );
+  });
+}
