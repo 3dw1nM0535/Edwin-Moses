3c94e7d3ee5eafdb7e21065024f08b0332c5e27d
3dw1nM0535 <workockmoses@gmail.com>
2018-04-02 17:15:59 +0300

    added projects/:id route

--- client/App.js
+++ client/App.js
@@ -5,6 +5,8 @@ import HomePage from "./component/pages/HomePage";
 import ProjectsPage from "./component/pages/ProjectsPage";
 import BlogPage from "./component/pages/BlogPage";
 import ContactPage from "./component/pages/ContactPage";
+import SkillsPage from "./component/pages/SkillsPage";
+import ProjectsListPage from "./component/pages/ProjectsListPage";
 
 import NavBar from "./component/navigation/NavBar";
 
@@ -13,8 +15,10 @@ const App = () => (
     <NavBar />
     <Route path="/" exact component={HomePage} />
     <Route path="/projects" exact component={ProjectsPage} />
+    <Route path="/projects/:title" exact component={ProjectsListPage} />
     <Route path="/contact" exact component={ContactPage} />
     <Route path="/blog" exact component={BlogPage} />
+    <Route path="/skills" exact component={SkillsPage} />
   </div>
 );
 
--- client/reducer/index.js
+++ client/reducer/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 import { routerReducer } from "react-router-redux";
 
 import user from "./user";
+import projects from "./projects";
 
 export default combineReducers({
   user,
+  projects,
   router: routerReducer,
 });
