cdc6fcd0dd76a6571b519969ce6a89ea6401e51b
3dw1nM0535 <workockmoses@gmail.com>
2018-04-03 14:40:23 +0300

    removed user reducer and updated project reducer to array data structure

--- client/reducer/index.js
+++ client/reducer/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
 import { routerReducer } from "react-router-redux";
 
-import user from "./user";
 import projects from "./projects";
 
 export default combineReducers({
-  user,
   projects,
   router: routerReducer,
 });
--- client/reducer/projects.js
+++ client/reducer/projects.js
@@ -1,6 +1,6 @@
 import { FIND_PROJECTS } from "../actions/types";
 
-export default function projects(state = {}, action = {}) {
+export default function projects(state = [], action = {}) {
   switch (action.type) {
     case FIND_PROJECTS:
       return action.projects;
