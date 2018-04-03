84ca33b07f06120cc626cfdf2032595756775c6c
3dw1nM0535 <workockmoses@gmail.com>
2018-04-03 16:40:06 +0300

    implemented contact form with validation and error messages

---
+++ client/component/forms/ContactForm.js
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+import { Form, Button } from "semantic-ui-react";
+import PropTypes from "prop-types";
+import isEmail from "validator/lib/isEmail";
+
+import InlineError from "../messages/InlineError";
+
+class ContactForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        email: "",
+        subject: "",
+        emailBody: "",
+      },
+      errors: {},
+      loading: false,
+    };
+  }
+
+  onChange = (e) => {
+    this.setState({
+      ...this.state,
+      data: { ...this.state.data, [e.target.name]: e.target.value },
+    });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    const errors = this.validate(this.state.data);
+    this.setState({ errors });
+    if (Object.keys(errors).length === 0) {
+      this.setState({ loading: true });
+      this.props.submit(this.state.data);
+    }
+  }
+
+  validate = (data) => {
+    const errors = {};
+
+    if (!data.email) errors.email = "Can't be Blank";
+    if (!isEmail(data.email)) errors.email = "Invalid Email address";
+    if (!data.subject) errors.subject = "Can't be Blank";
+    if (!data.emailBody) errors.emailBody = "Can't be Blank";
+
+    return errors;
+  }
+
+  render() {
+    const { data, loading, errors } = this.state;
+
+    return (
+      <Form loading={loading} onSubmit={this.onSubmit} size="small">
+        <Form.Field error={errors.email}>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Enter your email address"
+            onChange={this.onChange}
+            value={data.email}
+          />
+          { errors.email && <InlineError text={errors.email} /> }
+        </Form.Field>
+        <Form.Field error={errors.subject}>
+          <input
+            type="text"
+            id="subject"
+            name="subject"
+            placeholder="Subject of Email"
+            onChange={this.onChange}
+            value={data.subject}
+          />
+          { errors.subject && <InlineError text={errors.subject} /> }
+          </Form.Field>
+          <Form.Field error={errors.emailBody}>
+            <textarea onChange={this.onChange} placeholder="Enter email body" value={data.emailBody} name="emailBody"></textarea>
+            { errors.emailBody && <InlineError text={errors.emailBody} /> }
+          </Form.Field>
+          <Button color="black" content="Send" />
+      </Form>
+    );
+  }
+}
+
+ContactForm.propTypes = {
+  submit: PropTypes.func.isRequired,
+};
+
+export default ContactForm;
--- client/component/pages/ContactPage.js
+++ client/component/pages/ContactPage.js
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
 import { Grid, Header } from "semantic-ui-react";
 
+import ContactForm from "../forms/ContactForm";
+
 class ContactPage extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
 
+  submit = data => console.log(data);
+
   render() {
     return (
-      <div></div>
+      <Grid stackable centered className="padding">
+        <Grid.Column width={7}>
+          <Header as="h1">Contact Form</Header>
+          <ContactForm submit={this.submit} />
+        </Grid.Column>
+      </Grid>
     );
   }
 }
