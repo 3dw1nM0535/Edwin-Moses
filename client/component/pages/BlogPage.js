318d280f402811567d18accc4aba2056430cdba8
3dw1nM0535 <workockmoses@gmail.com>
2018-04-03 15:38:41 +0300

    styled blog page for future render

--- client/component/pages/BlogPage.js
+++ client/component/pages/BlogPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Grid, Header } from "semantic-ui-react";
+import countdownTimer from "../../utils/countDownTimer";
 
 class BlogPage extends Component {
   constructor(props) {
@@ -7,8 +8,14 @@ class BlogPage extends Component {
     this.state = {};
   }
   render() {
+    countdownTimer();
     return (
-      <div></div>
+      <Grid centered>
+        <Header className="coming-soon" as="h1" size="huge">Coming Soon</Header>
+        <Grid.Row>
+          <Header id="countdown" as="h1"></Header>
+        </Grid.Row>
+      </Grid>
     );
   }
 }
