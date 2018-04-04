3f2912be3c75c873b9ad44657b4468c1a0fcf312
3dw1nM0535 <workockmoses@gmail.com>
2018-04-04 13:24:50 +0300

    removed countdowntimer.js and implemented countdown in component lifecycle

--- client/component/pages/BlogPage.js
+++ client/component/pages/BlogPage.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Grid, Header } from "semantic-ui-react";
-import countdownTimer from "../../utils/countDownTimer";
 
 class BlogPage extends Component {
   constructor(props) {
@@ -8,8 +7,37 @@ class BlogPage extends Component {
     this.state = {};
   }
 
+  componentDidMount = () => {
+    this.timer = setInterval(this.tick, 1000);
+  }
+
+  componentWillUnmount = () => {
+    clearInterval(this.timer);
+  }
+
+  tick = () => {
+    // get count down date
+    const countDownDate = new Date("May 19, 2018 15:37:25").getTime();
+    // get curent down
+    const now = new Date().getTime();
+    // calculate distance between count down date and current date
+    const distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    //display result in div element
+    document.getElementById("countdown").innerHTML = `${days}d ${hours}h ${minutes}min ${seconds}s`;
+
+    if (distance < 0) {
+      document.getElementById("countdown").innerHTML = "";
+    }
+  }
+
   render() {
-    countdownTimer();
     return (
       <Grid centered>
         <Header className="coming-soon" as="h1" size="huge">Coming Soon</Header>
