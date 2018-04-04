4960f9b2661e3bc177f47a31825990f4f484c9a0
3dw1nM0535 <workockmoses@gmail.com>
2018-04-04 14:47:20 +0300

    redefined thunk, logger middleware

--- client/store/store.js
+++ client/store/store.js
@@ -2,20 +2,20 @@ import { createStore, applyMiddleware } from "redux";
 import { routerMiddleware } from "react-router-redux";
 import { createLogger } from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
+import thunkMiddleware from "redux-thunk";
 import createHistory from "history/createBrowserHistory";
 
 import rootReducer from "../reducer/index";
 
 export const history = createHistory();
 
-const logger = createLogger();
+const loggerMiddleware = createLogger();
 const historyMiddleware = routerMiddleware(history);
 
 export default function configureStore() {
   const store = createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware(logger, thunk, historyMiddleware)),
+    composeWithDevTools(applyMiddleware(thunkMiddleware, loggerMiddleware, historyMiddleware)),
   );
 
   return store;
